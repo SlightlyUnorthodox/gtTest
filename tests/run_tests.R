80533363ba20ffaa880a5f1638a1e784763a3f83
SlightlyUnorthodox <gerts.dax1@gmail.com>
2017-02-23 16:53:34 -0500

    Designed test run script using RUnit, added preliminary tests for various groupby calls

---
+++ tests/run_tests.R
@@ -0,0 +1,65 @@
+# Author: Dax Gerts (2017)
+# Description: The main test script for gtTest
+
+library(RUnit)
+
+# Define testable libraries
+all.testable.libraries <- c(
+    "gtBase",
+    "gtStats",
+    "gtSampling",
+    "gtTranslator"
+)
+
+# Parse script input
+args <- commandArgs(trailingOnly = TRUE)
+if (is.na(args[1]) || nchar(as.character(args[1]))) {
+  libraries.to.test <- all.testable.libraries
+} else {
+    libraries.to.test <- trimws(unlist(strsplit(args[1],",")))
+}
+
+print(libraries.to.test)
+
+# Define functions
+
+DisplayEnvironmentInformation <- function() {
+    # Display list of pertinent system information
+    cat("System mode:", Sys.getenv("mode"), "\n")
+}
+
+CheckTestableLibraries <- function(libs, testable.libs) {
+    verified.list.of.libs <- character()
+
+    # Confirm lib is testable
+    for(lib in libs) {
+        if(lib %in% testable.libs) {
+            verified.list.of.libs <- c(verified.list.of.libs, lib)
+        } else {
+            warning("Library,'", lib, "', is not a testable library.\n", sep = "")
+        }
+    }
+    return(verified.list.of.libs)
+}
+
+RunTests <- function(libs = all.testable.libraries) {
+    # For each testable library, run all .R files in directory
+    path.names <- character()
+
+    for(lib in libs) {
+        path.names <- c(path.names, paste0(lib, "Tests"))   
+    }
+
+    test.suite <- defineTestSuite("Grokit Build Test", dirs = file.path(path.names), testFileRegexp = "\\.[rR]$")
+
+    test.batch.results <- runTestSuite(test.suite)
+
+    printTextProtocol(test.batch.results)
+}
+
+# Execute statements
+DisplayEnvironmentInformation()
+
+libraries.to.test <- CheckTestableLibraries(libraries.to.test, all.testable.libraries)
+
+RunTests(libraries.to.test)
